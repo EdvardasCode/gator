c105f09b8e1e3b8bdadd3af634587d0cb9604015
Edvardas <edvardas.kozlovas@gmail.com>
2026-04-06 14:45:21 +0100

    chore: add schema and SQL queries

---
+++ sql/queries/feeds.sql
@@ -0,0 +1,15 @@
+-- name: AddFeed :one
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6
+)
+RETURNING *;
+
+-- name: FetchFeeds :many
+SELECT f.name, f.url, u.name AS user_name FROM feeds AS f
+JOIN users AS u ON f.user_id = u.id;
